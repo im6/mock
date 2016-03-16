bca74de9bb4aa3e587a0b9554b5fdc1234b1de36
jinouwuque <ee2win@gmail.com>
2016-03-15 23:14:21 -0400

    add message input bar

--- src/app/datetree/modules/chat/controllers/chat_mainCtrl.js
+++ src/app/datetree/modules/chat/controllers/chat_mainCtrl.js
@@ -3,7 +3,16 @@ angular.module('app')
         '$scope',
         function($scope){
             _.merge($scope,{
-                test: "this is chat controller"
+                test: "this is chat controller",
+                msglist:[
+                    {msg: "Hey how's it going?", me: false},
+                    {msg: "Just send message?", me: true},
+                    {msg: "Cool", me: false},
+                    {msg: "Soemtime I do that too", me: true},
+                    {msg: "I am kinda going blue today", me: false},
+                    {msg: "Hey how's going", me: true},
+                    {msg: "pretty well, thnks", me: false}
+                ]
             });
         }
     ]);
