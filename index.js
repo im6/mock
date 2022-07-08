353c38621f4c50e7739b2358b2911919bc68e885
im6 <ee2win@gmail.com>
2022-07-07 22:47:56 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+// const cors = require("cors");
 
+// app.use(cors());
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.json({
+    text: "Hello World",
+  });
 });
 
 app.listen(3000);
